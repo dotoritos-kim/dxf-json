2d572d556d17cf780de19f16756aa5611f89a430
Phryxia <xahhaepica@gmail.com>
2024-01-21 19:58:05 +0900

    Remove unused files and functions

--- src/types/entity.ts
+++
@@ -1,7 +0,0 @@
-import type { Matrix3 } from 'three';
-
-export interface CommonEntityDecomposeOption {
-    layer?: string;
-    color: number;
-    extrusionTransform?: Matrix3;
-}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,7 @@
-export * from './binarySearch';
-export * from './disjointSet';
-export * from './graph';
-export * from './functional';
 export * from './flooding';
-export * from './triangle';
-export * from './queue';
 
-import { reduce } from '@fxts/core';
 import { Vector2, Vector3 } from 'three';
-import type { Bound, Point2D, Point3D } from '../types';
+import type { Point2D, Point3D } from '../types';
 
 export function pointToVector2(point: Point3D | Point2D): Vector2 {
     return new Vector2(point.x, point.y);
@@ -16,15 +9,6 @@ export function pointToVector2(point: Point3D | Point2D): Vector2 {
 
 export function pointToVector3(point: Point3D | Point2D): Vector3 {
     return new Vector3(point.x, point.y, (point as Point3D).z ?? 0);
-}
-
-export function swapVector(v1: Vector2, v2: Vector2): void {
-    let temp = v1.x;
-    v1.x = v2.x;
-    v2.x = temp;
-    temp = v1.y;
-    v1.y = v2.y;
-    v2.y = temp;
 }
 
 export function classify<T>(
@@ -43,28 +27,6 @@ export function classify<T>(
     return result;
 }
 
-export function updateBounds(v: Point2D, bounds?: Bound) {
-    bounds ??= { minX: v.x, maxX: v.x, minY: v.y, maxY: v.y };
-    bounds.minX = Math.min(bounds.minX, v.x);
-    bounds.maxX = Math.max(bounds.maxX, v.x);
-    bounds.minY = Math.min(bounds.minY, v.y);
-    bounds.maxY = Math.max(bounds.maxY, v.y);
-    return bounds;
-}
-
-export function getBoundBox(vertices: Iterable<Point2D>) {
-    return reduce(
-        (bound, v) => updateBounds(v, bound),
-        {
-            minX: Infinity,
-            maxX: -Infinity,
-            minY: Infinity,
-            maxY: -Infinity,
-        },
-        vertices,
-    );
-}
-
 /**
  * 정수 생성기
  *
@@ -79,24 +41,5 @@ export function* generateIntegers(
 ) {
     for (let n = start; n !== end; n += increment) {
         yield n;
-    }
-}
-
-export function swap<T>(list: T[], i1: number, i2: number) {
-    const temp = list[i1];
-    list[i1] = list[i2];
-    list[i2] = temp;
-}
-
-export function pushIfNotEqual(v: Vector2, vertices: Vector2[]) {
-    if (!vertices.length) {
-        vertices.push(v);
-        return;
-    }
-
-    const u = vertices.at(-1)!;
-
-    if (v.x !== u.x || v.y !== u.y) {
-        vertices.push(v);
     }
 }
