298cc831ee79a2f1fbe9169f34c411b1be607055
Phryxia <xahhaepica@gmail.com>
2025-08-11 11:36:22 +0900

    chore: add unit test for DICTIONARY parser

---
+++ src/parser/objects/dictionary/parser.test.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { describe, expect, test } from "vitest";
+import { DxfArrayScanner } from '../../DxfArrayScanner';
+import { createParser } from '../../shared/parserGenerator';
+import { DictionarySnippets } from './parser';
+
+describe("DICTIONARY parser", () => {
+    test('tc0', () => {
+      const content = readFileSync(join(__dirname, "./tc0.partial_dxf"), "utf-8");
+      const scanner = new DxfArrayScanner(content.split("\n"));
+      const parser = createParser(DictionarySnippets);
+
+      let curr = scanner.next()
+      curr = scanner.next(); // skip 0
+
+      const obj: any = {}
+
+      parser(curr, scanner, obj)
+
+      expect(obj).toMatchObject({
+        handle: "2D2",
+        ownerObjectId: "2C6",
+        subclassMarker: "AcDbDictionary",
+        isHardOwned: true,
+        recordCloneFlag: 1,
+        entries: [
+          {
+            name: "ACAD_FILTER",
+            objectHardId: "2D3"
+          }
+        ]
+      })
+    })
+});
