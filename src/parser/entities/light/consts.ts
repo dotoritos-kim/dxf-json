a51acc7ee5c393b84d22839e56094595fae1ec2a
Phryxia <xahhaepica@gmail.com>
2025-12-08 23:58:30 +0900

    feat: implement LIGHT entity parser

---
+++ src/parser/entities/light/consts.ts
@@ -0,0 +1,16 @@
+export enum LightType {
+  Distant = 1,
+  Point = 2,
+  Spot = 3,
+}
+
+export enum AttenuationType {
+  None = 0,
+  InverseLinear = 1,
+  InverseSquare = 2,
+}
+
+export enum ShadowType {
+  RayTrace = 0,
+  ShadowMap = 1,
+}
